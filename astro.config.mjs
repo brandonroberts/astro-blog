f74350a21788aff5eb5767fd116b33647e898fa6
Brandon <robertsbt@gmail.com>
2022-04-13 22:23:17 -0500

    feat: upgrade to Astro 1.0.0-beta.10 (#1)
    https://docs.astro.build/en/migrate/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: 'https://brandonroberts.dev',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-    tailwindConfig: './tailwind.config.js',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
-  },
-  renderers: [],
-};
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://brandonroberts.dev',
+  integrations: [tailwind(), sitemap()]
+});
--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
-  // more options here
-};
